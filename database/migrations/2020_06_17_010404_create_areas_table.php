d8c0fe30142fb99839f19f8bfa050adaf0fc5672
fendergx <fendergx@gmail.com>
2020-06-26 22:37:57 -0600

    creadas las migraciones para comenzar a trabajar la bd

---
+++ database/migrations/2020_06_17_010404_create_areas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAreasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('areas', function (Blueprint $table) {
+            $table->bigIncrements('id_area');
+            $table->string('nombre_area')->unique();
+            //llave foranea
+            $table->unsignedBigInteger('id_coord');
+            $table->foreign('id_coord')->references('id_coord')->on('coordinaciones');
+            //creado y actualizado
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('areas');
+    }
+}
---
+++ database/migrations/2020_07_01_000000_create_usuarios_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+//para HASH
+use Illuminate\Support\Facades\Hash;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->bigIncrements('id_usuario');
+            $table->string('nombre_usuario')->unique();
+            $table->string('password',1024);
+            $table->string('nombres');
+            $table->string('apellidos');
+            //llaves foráneas
+            $table->unsignedBigInteger('id_rol');
+            $table->foreign('id_rol')->references('id_rol')->on('roles');
+            $table->unsignedBigInteger('id_coord')->nullable();
+            $table->foreign('id_coord')->references('id_coord')->on('coordinaciones');
+            $table->unsignedBigInteger('id_area')->nullable();
+            $table->foreign('id_area')->references('id_area')->on('areas');
+            //creado y actualizado a
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
