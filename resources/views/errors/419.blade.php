d3df16a2c39115cff581187f6e3408bc5da02a3f
david071993 <davicin.rvr.14@gmail.com>
2020-09-03 21:44:34 -0600

    paginas de error 419 y 429

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Error 419!</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/errors.css') }}">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">
+</head>
+<body>
+	<div id="main">
+    	<div class="fof">
+        		<h1>Error 419</h1>
+        		<h2>Esta página ha expirado, vuelva a ingresar.</h2>
+        		<h2 class="text-center"><a class="btn btn-outline-success my-2 my-sm-0" href="{{route('inicio.admin')}}">Inicio</a></h2>
+    	</div>
+</div>
+</body>
+</html>
