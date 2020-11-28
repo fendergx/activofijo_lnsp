a0a3d531748e51cdeedfebf5f65cfb71bc6dd5ae
fendergx <fendergx@gmail.com>
2020-11-28 02:16:00 -0600

    archivos de js y css offline
    ahora bootstrap y las demás librerias trabajan offline, sin conexión a internet
    - se creó un par de archivos backup, para trabajar online

--- resources/views/base/head_online.blade.php
+++ resources/views/base/head_online.blade.php
@@ -1,8 +1,9 @@
+<!-- para trabajar con css online -->
 <!-- Required meta tags -->
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <!-- Bootstrap CSS -->
-<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">
 
 <!-- Custom styles for this template -->
 <link href="{{ asset('css/sticky-footer-navbar.css') }}" rel="stylesheet">
@@ -10,5 +11,6 @@
 
 <!-- toastr notifications -->
 <link rel="stylesheet" href="{{ asset('toastr/toastr.min.css') }}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
 <!-- Font Awesome -->
-<link rel="stylesheet" href="{{asset('css/fontawesome.all.min.css')}}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
