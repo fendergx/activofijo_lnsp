372f2f118460426115c7a5564942d60c9edf14a1
fendergx <fendergx@gmail.com>
2021-01-04 01:46:35 -0600

    plantilla de construccion de seccion

---
+++ resources/views/_construction.blade.php
@@ -0,0 +1,10 @@
+<div class="text-center">
+	<h1>Sección en construcción</h1>
+	<p>
+		<h3>Esta sección se encuentra en desarrollo 
+			<br> y se habilitará en cuanto esté terminada
+			<br><br>
+		</h3>
+		<i class="fas fa-business-time fa-10x"></i>	&nbsp; 
+	</p>
+</div>
