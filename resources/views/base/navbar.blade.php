b18bf6e53cebb109d57eca850d3e7f21d63d47e1
fendergx <fendergx@gmail.com>
2021-01-06 04:40:39 -0600

    Update navbar.blade.php
    cambiado el navbar por roles de usuario

--- resources/views/base/navbar.blade.php
+++ resources/views/base/navbar.blade.php
@@ -10,84 +10,138 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
           <ul class="navbar-nav mr-auto">
+            @auth
+            
+            @if(Auth::user()->id_rol == 1)
+            <!--Para administrador de sistema -->
             <li class="nav-item">
-              <!--<a class="nav-link" href="#">test</a>-->
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Administración</a>
-              <div class="dropdown-menu dropdown-menu-right animate slideIn shadow" aria-labelledby="dropdown01">
-                <a class="dropdown-item" href="{{route('usuario.registrar')}}">Registrar usuario</a>
-                <a class="dropdown-item" href="{{ route('usuario.lista') }}">Usuarios</a>
-                <a class="dropdown-item" href="{{ route('coordinacion.index') }}">Coordinaciones</a>
-                <a class="dropdown-item" href="{{ route('area.index') }}">Áreas</a>
-              </div>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Preparaduría</a>
-              <div class="dropdown-menu dropdown-menu-right animate slideIn shadow" aria-labelledby="dropdown01">
-                <a class="dropdown-item" href="{{ route('reactivo.index') }}">Reactivos</a>
-                <a class="dropdown-item" href="{{ route('cliente.index') }}">Clientes</a>
-                <a class="dropdown-item" href="{{ route('persona.index') }}">Personas responsables</a>
-
-              </div>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Activo Fijo</a>
-              <div class="dropdown-menu dropdown-menu-right animate slideIn shadow" aria-labelledby="dropdown01">
-                <a class="dropdown-item" href="{{ route('activofijo.index') }}">Activo Fijo</a>
-                <a class="dropdown-item" href="{{ route('fuente.index') }}">Fuentes proveedoras</a>
-                <a class="dropdown-item" href="{{ route('color.index') }}">Colores</a>
-                <a class="dropdown-item" href="{{ route('estado.index') }}">Estados de A.F.</a>
-                <a class="dropdown-item" href="{{ route('ubicacion.index') }}">Ubicaciones</a>
-              </div>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Formularios</a>
-              <div class="dropdown-menu dropdown-menu-right animate slideIn shadow" aria-labelledby="dropdown01">
-                <a class="dropdown-item" href="{{ route('form.a') }}">Formulario A</a>
-                <a class="dropdown-item" href="{{ route('form.b') }}">Formulario B</a>
-                <a class="dropdown-item" href="{{ route('form.c') }}">Formulario C</a>
-                <a class="dropdown-item" href="{{ route('form.d') }}">Formulario D</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="{{ route('acta') }}">Acta de entrega</a>
-                <a class="dropdown-item" href="{{ route('salida') }}">Orden de salida</a>
-                <a class="dropdown-item" href="{{ route('asignacion') }}">Asignación de Mobiliario</a>
-              </div>
+              <a class="nav-link" href="{{route('usuario.registrar')}}">Registrar usuario <i class="fas fa-user-plus" aria-hidden="true"></i></a> 
             </li>
             <li class="nav-item">
-              <!--<a class="nav-link disabled" href="#">LinkDeshabilitado</a>-->
-            </li>
-          </ul>
-          @auth
-          <!-- Right Side Of Navbar -->
-          <ul class="navbar-nav ml-auto">
-            <!-- Authentication Links -->
-            <li class="nav-item dropdown">
-              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                <span class="caret"></span>
-                {{head(explode(' ', trim(Auth::user()->nombres))) }}  {{head(explode(' ', trim(Auth::user()->apellidos))) }} <span class="far fa-user"></span> </a>
+             <a class="nav-link" href="{{route('usuario.lista')}}">Usuarios</a> 
+           </li>
+           <li class="nav-item">
+             <a class="nav-link" href="{{route('coordinacion.index')}}">Coordinaciones</a> 
+           </li>
+           <li class="nav-item">
+             <a class="nav-link" href="{{route('area.index')}}">Áreas</a> 
+           </li>
+           @endif
 
-                <div class="dropdown-menu dropdown-menu-right animate slideIn shadow" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item text-right" href="{{ route('usuario.perfil') }}">
-                    Mi perfil &nbsp;<i class="fas fa-info"></i>
-                  </a>
-                  <a class="dropdown-item text-right" href="{{ route('logout') }}"
-                  onclick="event.preventDefault();
-                  document.getElementById('logout-form').submit();">
-                  Cerrar sesión <i class="fas fa-user-lock" aria-hidden="true"></i>
+           @if(Auth::user()->id_rol == 2)
+           <!--Para encargado de activo fijo -->
+           <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Administración</a>
+            <div class="dropdown-menu dropdown-menu-right animate slideIn shadow" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="{{ route('fuente.index') }}">Fuentes proveedoras</a>
+              <a class="dropdown-item" href="{{ route('color.index') }}">Colores</a>
+              <a class="dropdown-item" href="{{ route('estado.index') }}">Estados de A.F.</a>
+              <a class="dropdown-item" href="{{ route('ubicacion.index') }}">Ubicaciones</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{route('activofijo.index')}}">Activo Fijo</a> 
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Solicitudes de movimiento</a> 
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Formularios</a>
+            <div class="dropdown-menu dropdown-menu-right animate slideIn shadow" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="{{ route('form.a') }}">Formulario A</a>
+              <a class="dropdown-item" href="{{ route('form.b') }}">Formulario B</a>
+              <a class="dropdown-item" href="{{ route('form.c') }}">Formulario C</a>
+              <a class="dropdown-item" href="{{ route('form.d') }}">Formulario D</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="{{ route('acta') }}">Acta de entrega</a>
+              <a class="dropdown-item" href="{{ route('salida') }}">Orden de salida</a>
+              <a class="dropdown-item" href="{{ route('asignacion') }}">Asignación de Mobiliario</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Depreciación</a> 
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Historial</a>
+            <div class="dropdown-menu dropdown-menu-right animate slideIn shadow" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Movimientos internos</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Acta de entrega</a>
+              <a class="dropdown-item" href="#">Asignación de bienes</a>
+              <a class="dropdown-item" href="#">Orden de salida</a>
+            </div>
+          </li> 
+          @endif
+
+          @if(Auth::user()->id_rol == 3)
+          <!--Para encargado de preparaduría -->
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Administración</a>
+            <div class="dropdown-menu dropdown-menu-right animate slideIn shadow" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="{{ route('reactivo.index') }}">Reactivos</a>
+              <a class="dropdown-item" href="{{ route('cliente.index') }}">Clientes</a>
+              <a class="dropdown-item" href="{{ route('persona.index') }}">Personas responsables</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Ingresar pedido</a> 
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Informe de salida</a> 
+          </li>
+          @endif
+
+          @if(Auth::user()->id_rol == 4)
+          <!--Para jefe de laboratorio -->
+          <li class="nav-item">
+            <a class="nav-link" href="#">Activos fijos</a> 
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Solicitar activo fijo</a> 
+          </li>
+          @endif
+
+          @if(Auth::user()->id_rol == 5)
+          <!--Para jefe de area -->
+          <li class="nav-item">
+            <a class="nav-link" href="#">Activos fijos</a> 
+          </li>
+          @endif
+
+          @if(Auth::user()->id_rol == 6)
+          <!--Para usuario de activo fijo -->
+          <li class="nav-item">
+            <a class="nav-link" href="#">Activos fijos</a> 
+          </li>
+          @endif
+        </ul>
+        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav ml-auto">
+          <!-- Authentication Links -->
+          <li class="nav-item dropdown">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+              <span class="caret"></span>
+              {{head(explode(' ', trim(Auth::user()->nombres))) }}  {{head(explode(' ', trim(Auth::user()->apellidos))) }} <span class="far fa-user"></span> </a>
+
+              <div class="dropdown-menu dropdown-menu-right animate slideIn shadow" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item text-right" href="{{ route('usuario.perfil') }}">
+                  Mi perfil &nbsp;<i class="fas fa-info"></i>
                 </a>
-                <!--<a class="btn btn-outline-success my-2 my-sm-0" href="{{route('login')}}">Iniciar Sesion</a>-->
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                  @csrf
-                </form>
-              </div>
-            </li>
-          </ul>
-          @endauth
-          @guest
-          <!-- Código si hubiera invitado -->
-          @endguest
-        </div>
-      </nav>
-    </header>
+                <a class="dropdown-item text-right" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">
+                Cerrar sesión <i class="fas fa-user-lock" aria-hidden="true"></i>
+              </a>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+              </form>
+            </div>
+          </li>
+        </ul>
+        @endauth
+        @guest
+        <!-- Código si hubiera invitado -->
+        @endguest
+      </div>
+    </nav>
+  </header>
