42f6eabebb00f64990ed90a408acfc612397db2c
fendergx <fendergx@gmail.com>
2021-01-06 13:31:39 -0600

    Update ExcelController.php
    restricción de exportación de excel solo para encargado de AF

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -6,10 +6,15 @@ use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 
 use App\Exports\ActivoFijoExport;
+use Illuminate\Support\Facades\Auth;
 
 class ExcelController extends Controller
 {
-    public function exportarExcel(){
-    	return Excel::download(new ActivoFijoExport, 'activos_fijos.xlsx');
-    }
+	public function exportarExcel(){
+		if(Auth::user()->id_rol == 2){
+			return Excel::download(new ActivoFijoExport, 'activos_fijos.xlsx');
+		}else{
+			return abort(403);
+		}
+	}
 }
