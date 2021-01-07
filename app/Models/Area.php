68237b9a8bdec3e047649b8d76c4be7f60295680
fendergx <fendergx@gmail.com>
2021-01-07 01:31:30 -0600

    comentado Modelo Area, creados usuarios base
    admin
    admin.activo
    admin.preparaduria
    coordinador
    jefe.area
    usuario
    
    todos con pass 'admin'

--- app/Models/Area.php
+++ app/Models/Area.php
@@ -2,19 +2,24 @@
 
 namespace App\Models;
 
+//uso de la libreria Model
 use Illuminate\Database\Eloquent\Model;
 
 class Area extends Model
 {
-    protected $table='areas';
-    protected $primaryKey='id_area';
+	//nombre de la tabla
+	protected $table='areas';
+	//llave primaria de la tabla (si existe)
+	protected $primaryKey='id_area';
 
-    protected $fillable = ['nombre_coord','id_coord'];
+	//atributos de la tabla
+	protected $fillable = ['nombre_coord','id_coord'];
 
-    //se usa para cuando la tabla no tiene TimeStamp (created_at updated_at)
-    public $timestamps = false;
+    //definición si existen timestamp en la tabla (created_at updated_at)
+	public $timestamps = false;
 
-    public function coordinacion(){
-    	return $this->belongsTo(Coordinacion::class, 'id_coord','id_coord');
-    }
+	//funciones de relación con otras tablas
+	public function coordinacion(){
+		return $this->belongsTo(Coordinacion::class, 'id_coord','id_coord');
+	}
 }
--- database/seeds/UsuariosSeeder.php
+++ database/seeds/UsuariosSeeder.php
@@ -18,78 +18,118 @@ class UsuariosSeeder extends Seeder
     public function run()
     {
 
-        User::create(['nombre_usuario' => 'admin',
-            'password' => Hash::make('admin'),
-            'nombres' => 'Super',
-            'apellidos' => 'Administrador',
-            'id_rol' => '1',
-            
-        ]);
+    	User::create(['nombre_usuario' => 'admin',
+    		'password' => Hash::make('admin'),
+    		'nombres' => 'Super',
+    		'apellidos' => 'Administrador',
+    		'id_rol' => '1',
 
-        User::create(['nombre_usuario' => 'karen.penate',
-        	'password' => Hash::make('karen'),
-        	'nombres' => 'Karen Elvira',
-        	'apellidos' => 'Peñate Aviles',
-        	'id_rol' => '1',
+    	]);
 
-        ]);
+    	User::create(['nombre_usuario' => 'admin.activo',
+    		'password' => Hash::make('admin'),
+    		'nombres' => 'Administrador',
+    		'apellidos' => 'AF',
+    		'id_rol' => '2',
+    	]);
 
-        User::create(['nombre_usuario' => 'fernando.aguilar',
-            'password' => Hash::make('jose.com'),
-            'nombres' => 'José Fernando',
-            'apellidos' => 'Aguilar Rodríguez',
-            'id_rol' => '1',
+    	User::create(['nombre_usuario' => 'admin.preparaduria',
+    		'password' => Hash::make('admin'),
+    		'nombres' => 'Administrador',
+    		'apellidos' => 'AF',
+    		'id_rol' => '3',
+    	]);
 
-        ]);
+    	User::create(['nombre_usuario' => 'coordinador',
+    		'password' => Hash::make('admin'),
+    		'nombres' => 'Jefe de',
+    		'apellidos' => 'Coordinación',
+    		'id_rol' => '4',
+    		'id_coord' => '1',
+    	]);
 
-        User::create(['nombre_usuario' => 'martell.xiomara',
-            'password' => Hash::make('martell.xiomara'),
-            'nombres' => 'Xiomara Cecilia',
-            'apellidos' => 'Martell',
-            'id_rol' => '2',
-        ]);
+    	User::create(['nombre_usuario' => 'jefe.area',
+    		'password' => Hash::make('admin'),
+    		'nombres' => 'Jefe de',
+    		'apellidos' => 'Coordinación',
+    		'id_rol' => '5',
+    		'id_coord' => '1',
+    		'id_area' => '1',
+    	]);
 
-        User::create(['nombre_usuario' => 'omar.orellana',
-        	'password' => Hash::make('omar'),
-        	'nombres' => 'Omar Enrique',
-        	'apellidos' => 'Cruz Orellana',
-        	'id_rol' => '2',
-        ]);
+    	User::create(['nombre_usuario' => 'usuario',
+    		'password' => Hash::make('admin'),
+    		'nombres' => 'usuario',
+    		'apellidos' => 'de AF',
+    		'id_rol' => '6',
+    		'id_coord' => '1',
+    		'id_area' => '1',
+    	]);
 
-        User::create(['nombre_usuario' => 'javier.menendez',
-        	'password' => Hash::make('javier'),
-        	'nombres' => 'Javier',
-        	'apellidos' => 'Menendez',
-        	'id_rol' => '3',
-        ]);
+    	User::create(['nombre_usuario' => 'karen.penate',
+    		'password' => Hash::make('karen'),
+    		'nombres' => 'Karen Elvira',
+    		'apellidos' => 'Peñate Aviles',
+    		'id_rol' => '1',
 
-        User::create(['nombre_usuario' => 'david.jimenez',
-        	'password' => Hash::make('david'),
-        	'nombres' => 'José David',
-        	'apellidos' => 'Jiménez',
-        	'id_rol' => '4',
-        	'id_coord' => '1',
-        ]);
+    	]);
 
-        User::create([
-            'nombre_usuario' => 'manuel.chacon',
-            'password' => Hash::make('manuel'),
-            'nombres' => 'José Manuel',
-            'apellidos' => 'Chacón',
-            'id_rol' => '5',
-            'id_coord' => '1',
-            'id_area' => '1',
-        ]);
-        
+    	User::create(['nombre_usuario' => 'fernando.aguilar',
+    		'password' => Hash::make('jose.com'),
+    		'nombres' => 'José Fernando',
+    		'apellidos' => 'Aguilar Rodríguez',
+    		'id_rol' => '1',
 
-        User::create(['nombre_usuario' => 'francisco.gomez',
-        	'password' => Hash::make('francisco'),
-        	'nombres' => 'Francisco',
-        	'apellidos' => 'Gómez',
-            'cargo' => 'tester',
-            'id_rol' => '6',
-            'id_coord' => '1',
-            'id_area' => '1',
-        ]);
+    	]);
+
+    	User::create(['nombre_usuario' => 'martell.xiomara',
+    		'password' => Hash::make('martell.xiomara'),
+    		'nombres' => 'Xiomara Cecilia',
+    		'apellidos' => 'Martell',
+    		'id_rol' => '2',
+    	]);
+
+    	User::create(['nombre_usuario' => 'omar.orellana',
+    		'password' => Hash::make('omar'),
+    		'nombres' => 'Omar Enrique',
+    		'apellidos' => 'Cruz Orellana',
+    		'id_rol' => '2',
+    	]);
+
+    	User::create(['nombre_usuario' => 'javier.menendez',
+    		'password' => Hash::make('javier'),
+    		'nombres' => 'Javier',
+    		'apellidos' => 'Menendez',
+    		'id_rol' => '3',
+    	]);
+
+    	User::create(['nombre_usuario' => 'david.jimenez',
+    		'password' => Hash::make('david'),
+    		'nombres' => 'José David',
+    		'apellidos' => 'Jiménez',
+    		'id_rol' => '4',
+    		'id_coord' => '1',
+    	]);
+
+    	User::create([
+    		'nombre_usuario' => 'manuel.chacon',
+    		'password' => Hash::make('manuel'),
+    		'nombres' => 'José Manuel',
+    		'apellidos' => 'Chacón',
+    		'id_rol' => '5',
+    		'id_coord' => '1',
+    		'id_area' => '1',
+    	]);
+
+
+    	User::create(['nombre_usuario' => 'francisco.gomez',
+    		'password' => Hash::make('francisco'),
+    		'nombres' => 'Francisco',
+    		'apellidos' => 'Gómez',
+    		'cargo' => 'tester',
+    		'id_rol' => '6',
+    		'id_coord' => '1',
+    		'id_area' => '1',
+    	]);
     }
 }
